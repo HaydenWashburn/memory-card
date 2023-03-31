9c0120c542300472ff7b76c7825a4e04ea5a9ee1
HaydenWashburn <hayden.washburn1@gmail.com>
2023-03-31 08:55:07 -0500

    created function to shuffle cards each time a game is started

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,25 +1,32 @@
+import {useState} from 'react'
 import "./App.css";
-import { Component } from 'react';
-import Header from "./components/header";
-import Game from "./components/game.jsx";
-class  App extends Component {
-  constructor(){
-    super();
 
-    this.state = {
-      currentScore: 0,
-      bestScore: 0
-    }
+const cardImages = [
+  {"src": "./public/gryffindor-img.webp"},
+  {"src": "./public/hufflepuff-img.png"},
+  {"src": "./public/Ravenclaw-img.png"},
+  {"src": "./public/slytherin-img.png"}
+]
+
+function App(props) {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+    .sort(() => Math.random() - 0.5)
+    .map((card) => ({...card, id: Math.random()}))
+
+    setCards(shuffledCards)
+    setTurns(0)
   }
-
-  render(){
-    const { currentScore, bestScore } = this.state;
+  console.log(cards, turns)
   return (
-    <div className="App">
-      <Header currentScore = {currentScore} bestScore = {bestScore} />
-      <Game />
+    <div className="App-header">
+  <h1>Magic Match</h1>
+  <button onClick ={shuffleCards}>New Game</button>
     </div>
-  )};
+  );
 }
 
 export default App;
