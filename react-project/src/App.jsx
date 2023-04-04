aa3a0d7952c35a4565d2bd82764b874f799afc51
HaydenWashburn <hayden.washburn1@gmail.com>
2023-04-04 17:36:37 -0500

    .

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,7 +1,7 @@
 import { Component } from 'react'
 import "./App.css";
-import  Header  from './components/Header';
-import  Game  from './components/Game'
+import  Header  from './Header';
+import  Game  from './Game'
 
 class App extends Component{
 constructor(){
