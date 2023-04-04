db49cb7ecba923e037faf3613dbcc587eeb7a50e
HaydenWashburn <hayden.washburn1@gmail.com>
2023-04-03 19:30:13 -0500

    restarted project from beginning

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,30 +1,14 @@
-import {useState} from 'react'
 import "./App.css";
-
-const cardImages = [
-  {"src": "./public/gryffindor-img.webp"},
-  {"src": "./public/hufflepuff-img.png"},
-  {"src": "./public/Ravenclaw-img.png"},
-  {"src": "./public/slytherin-img.png"}
-]
-
+import  Cards  from './components/cards'
 function App(props) {
-  const [cards, setCards] = useState([])
-  const [turns, setTurns] = useState(0)
-
-  const shuffleCards = () => {
-    const shuffledCards = [...cardImages, ...cardImages]
-    .sort(() => Math.random() - 0.5)
-    .map((card) => ({...card, id: Math.random()}))
-
-    setCards(shuffledCards)
-    setTurns(0)
-  }
-  console.log(cards, turns)
+ 
   return (
     <div className="App-header">
-  <h1>Magic Match</h1>
-  <button onClick ={shuffleCards}>New Game</button>
+      <h1>Memory Card Game</h1>
+      <div className="container">
+      <Cards />
+
+      </div>
     </div>
   );
 }
---
+++ react-project/src/components/cards.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+
+function Cards(){
+    const [items, setItems] = useState([])
+    return(
+        <p>Cards</p>
+    )
+}
+
+export default Cards
